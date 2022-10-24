fe676f108e745436de50d89f45fd45c34bde877b
John McCaffery <john@intuitiveweb.co.uk>
2022-10-24 13:48:24 +0100

    InitialCommit

---
+++ API/GlobalUsing.cs
@@ -0,0 +1,15 @@
+global using Microsoft.EntityFrameworkCore;
+global using API.Entities;
+global using API.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API
+{
+    public class GlobalUsing
+    {
+        
+    }
+}
